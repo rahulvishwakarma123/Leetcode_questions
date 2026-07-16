7754c1ba8eb1da947c9af6b5f5121e9bded259e5
Rahul vishwakarma <148959639+rahulvishwakarma123@users.noreply.github.com>
2026-07-16 18:07:29 +0530

    Time: 6 ms (72.73%), Space: 45.1 MB (39.69%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,45 +1,37 @@
 class Solution {
-    public int totalSum = 0;
-    public int solve(int[] nums, int target, int sum, int n, int[][] dp){
-        if(n == 0){
-            if(target == sum){
-                return 1;
-            }else{
-                return 0;
-            }
-        }
-
-        // reuturn if already calculated
-        if(dp[n][sum + totalSum] != -1){
-            return dp[n][sum + totalSum];
-        }
-
-        // 2 choises are there - first add choise and second subtract choise
-        dp[n][sum + totalSum] = solve(nums, target, sum + nums[n-1], n-1, dp) + solve(nums, target, sum - nums[n-1], n-1, dp);
-        return dp[n][sum + totalSum];
-    }
     public int findTargetSumWays(int[] nums, int target) {
         int n = nums.length;
 
-        // pahale total sum calculate kiya
+        int sum = 0;
         for(int num : nums){
-            totalSum +=  num;
+            sum += num;
         }
 
-        // fir dp array banaya [n+1][2totalSum + 1] => 2totalsum because hamara sum negative me bhi ja sakta hai isliye jth index me sab jagah totalSum add kar denge jisse extreme negative sum 0 ho jaye;
-        // if sum = 5;
-        // then negative sum = -5;
-        // and positive sum = 5 hoga
-        // isi wajah se sab jagah second index me totalSum add hua hai -> index ko range me rakhane ke liye
-        int[][] dp = new int[n+1][totalSum + totalSum + 1];
+        if((sum + target) % 2 != 0 ||sum < Math.abs(target)){
+            return 0;
+        }
 
-        // initialization;
-        for(int i = 0; i <= n; i++){
-            for(int j = 0; j <= 2*totalSum; j++){
-                dp[i][j] = -1;
+        int s1 = (sum + target) / 2;
+
+        int[][] dp = new int[n+1][s1 + 1];
+
+        dp[0][0] = 1;
+        for(int i = 1; i <= n; i++){
+            dp[i][0] = 1; //sum = 0;
+        }
+
+        for(int i = 1; i <= n; i++){
+            for(int j = 0; j <= s1; j++){
+                // exclude
+                dp[i][j] = dp[i-1][j];
+
+                // include
+                if(nums[i-1] <= j){
+                    dp[i][j] += dp[i-1][j - nums[i-1]];
+                }
             }
         }
 
-        return solve(nums, target, 0, n, dp);
+        return dp[n][s1];
     }
 }
