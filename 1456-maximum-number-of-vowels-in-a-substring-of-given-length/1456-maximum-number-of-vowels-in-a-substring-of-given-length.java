c0978fac480559fc9c78163afbfb65a562ac03c4
Rahul vishwakarma <148959639+rahulvishwakarma123@users.noreply.github.com>
2026-08-18 20:14:21 +0530

    Time: 11 ms (91.53%), Space: 46.4 MB (65.61%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.java
@@ -1,20 +1,19 @@
 class Solution {
+    public boolean isVowel(String s, int i){
+        char c = s.charAt(i);
+        if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u'){
+            return true;
+        }
+        return false;
+    }
     public int maxVowels(String s, int k) {
-        HashMap<Character, Integer> hm = new HashMap<>();
-        hm.put('a', 'a' - 'a');
-        hm.put('e', 'e' - 'a');
-        hm.put('i', 'i' - 'a');
-        hm.put('o', 'o' - 'a');
-        hm.put('u', 'u' - 'a');
-
-
         int n = s.length();
         int ans = 0;
         int max = 0;
 
         // first window;
         for(int i = 0; i < k; i++){
-            if(hm.containsKey(s.charAt(i))){
+            if(isVowel(s, i)){
                 ans++;
             }
         }
@@ -23,10 +22,10 @@ class Solution {
 
         // slide the window;
         for(int i = k; i < n; i++){
-            if(hm.containsKey(s.charAt(i))){
+            if(isVowel(s, i)){
                 ans++;
             }
-            if(hm.containsKey(s.charAt(i-k))){
+            if(isVowel(s, i-k)){
                 ans--;
             }
 
