68d6610dac0a94ef99522419503ac1cf5159a6d8
Rahul vishwakarma <148959639+rahulvishwakarma123@users.noreply.github.com>
2026-08-29 15:25:47 +0530

    Time: 44 ms (13.53%), Space: 143.4 MB (87.81%) - LeetHub

--- 0239-sliding-window-maximum/0239-sliding-window-maximum.java
+++ 0239-sliding-window-maximum/0239-sliding-window-maximum.java
@@ -1,39 +1,23 @@
 class Solution {
     public int[] maxSlidingWindow(int[] nums, int k) {
-        Deque<Integer> dq = new ArrayDeque<>();
+        List<Integer> res = new ArrayList<>();
         int n = nums.length;
-        int[] res = new int[n-k+1];
-        int j = -1;
+        Deque<Integer> dq = new ArrayDeque<>(); // we store indexes instead of elements beacause we have to check that our element is in the window or not
 
-        // Analysing the first window
-        for(int i = 0; i < k; i++){
-            while(dq.size() > 0 && nums[dq.peekLast()] <= nums[i]){
-                dq.removeLast();
-            }
-            dq.addLast(i);
+        for(int i = 0; i < n; i++){
+            while(!dq.isEmpty() && dq.peekFirst() <= i - k) // remove the elements which are outside the window
+                dq.pollFirst();
+
+            while(!dq.isEmpty() && nums[dq.peekLast()] < nums[i]) // remove the elements which are less than current element because they are useless 
+            // while doing that we are maintaining a sorted order (desc) in our deque.
+                dq.pollLast();
+
+            dq.offerLast(i);
+
+            // window is >= k
+            if(i >= k-1)
+                res.add(nums[dq.peekFirst()]);
         }
-
-        for(int i = k; i < nums.length; i++){
-            // add the element of the previous window;
-            j++;
-            res[j] = nums[dq.peekFirst()];
-            // remove the element from dq of the last window
-            while(dq.size() > 0 && dq.peekFirst() <= i-k){
-                dq.removeFirst();
-            }
-            // remove the element from the dq which is less than curr element
-            while(dq.size() > 0 && nums[dq.peekLast()] <= nums[i]){
-                dq.removeLast();
-            }
-            //add the curr element to the deque
-            dq.addLast(i);
-        }
-
-        // add the element to the res of last window
-        j++;
-        res[j] = nums[dq.peekFirst()];
-
-        return res;
-
+        return res.stream().mapToInt(Integer :: intValue).toArray();
     }
 }
