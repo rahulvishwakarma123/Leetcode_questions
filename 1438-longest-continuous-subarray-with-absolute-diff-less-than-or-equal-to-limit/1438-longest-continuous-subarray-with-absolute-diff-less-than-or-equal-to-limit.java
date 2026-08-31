20e92f817faf0eb04d4dab7514218e9b9982f9fd
Rahul vishwakarma <148959639+rahulvishwakarma123@users.noreply.github.com>
2026-08-31 09:17:01 +0530

    Time: 30 ms (95.2%), Space: 86.8 MB (78.09%) - LeetHub

--- 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
+++ 1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit/1438-longest-continuous-subarray-with-absolute-diff-less-than-or-equal-to-limit.java
@@ -1,26 +1,29 @@
 class Solution {
-    
     public int longestSubarray(int[] nums, int limit) {
-        int i = 0,j; 
+        Deque<Integer> min = new ArrayDeque<>();
+        Deque<Integer> max = new ArrayDeque<>();
         int n = nums.length;
-        int res = 0;
-        // treemap to maintain the minimum and maximum frequency
-        // minimum on the ffirst position and maximum on the last position
-        TreeMap<Integer, Integer> map = new TreeMap<>();
-
-        //move the right pointer one by on in search of maximum window
+        int i = 0, j;
         for(j = 0; j < n; j++){
-            // expanding thee window by taking the elements one by one
-            map.put(nums[j], map.getOrDefault(nums[j],0) + 1);
-            while(map.lastEntry().getKey() - map.firstEntry().getKey() > limit){
-                map.put(nums[i], map.get(nums[i]) - 1);
-                if(map.get(nums[i]) == 0){
-                    map.remove(nums[i]);
-                }
+
+            // maintaining the order in min and max deque;
+            while(!min.isEmpty() && nums[j] < min.peekLast()){
+                min.pollLast();
+            }
+            while(!max.isEmpty() && nums[j] > max.peekLast()){
+                max.pollLast();
+            }
+
+            min.addLast(nums[j]);
+            max.addLast(nums[j]);
+
+            // agar curr i pe jo element hai ya to wo already remove ho gaye honge ya nahi hue honge to iska matalab wo ya to greatest ya lowest element in the window hai to ham unhe remove kar denge kyuki condition voilate kar rahi hai.
+            if(max.peek() - min.peek() > limit){
+                if(min.peek() == nums[i]) min.poll();
+                if(max.peek() == nums[i]) max.poll();
                 i++;
             }
-            res = Math.max(res, j - i + 1);
         }
-        return res;
+        return j - i;
     }
 }
