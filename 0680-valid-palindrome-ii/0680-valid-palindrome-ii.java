3e37ec934a67d16bbbca41b254a85c39251e01b0
Rahul vishwakarma <148959639+rahulvishwakarma123@users.noreply.github.com>
2026-09-07 17:22:19 +0530

    Time: 4 ms (98.77%), Space: 47.4 MB (93%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    // always think of the brute force approach first
+    public boolean validPalindrome(String s) {
+        int st = 0;
+        int end = s.length() -1;
+        while(st < end){
+            if(s.charAt(st) == s.charAt(end)){
+                st++;
+                end--;
+            }
+            else{
+                return isPalindrome(s, st + 1, end) || isPalindrome(s, st, end-1);
+            }
+        }
+        return true;
+    }
+    public boolean isPalindrome(String s, int st, int end){
+        while(st < end){
+            if(s.charAt(st) == s.charAt(end)){
+                st++;
+                end--;
+            }else return false;
+        }
+        return true;
+    }
+}
